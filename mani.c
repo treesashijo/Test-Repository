993ce2a832eb91db565f9c6b90e168661553cfa7
treesashijo <83066106+treesashijo@users.noreply.github.com>
2021-04-23 21:30:06 +0530

    Add files via upload

---
+++ mani.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int i=1;
+    for(i=1;i<10;i++)
+    {
+        if(i==5)
+        {
+            break;
+        }
+        printf("%d\t",i);
+    }
+    printf("\n\n");
+}
