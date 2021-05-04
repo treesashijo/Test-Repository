89b1036f5cd6a48e84cc817f6f5197f7afaa1ea5
treesashijo <83066106+treesashijo@users.noreply.github.com>
2021-05-04 21:45:14 +0530

    Add files via upload

---
+++ string1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    int n,i;
+    printf("What is your name? :");
+    scanf("%s",a);
+    printf("welcome\t");
+    printf("%s",a);
+    printf("\n");
+}
+
