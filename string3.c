f7e267a5143bd9b3db72d4f2b92dabfbe1bb848c
treesashijo <83066106+treesashijo@users.noreply.github.com>
2021-05-06 22:03:19 +0530

    Add files via upload

---
+++ string3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    int length=0,i,j;
+    printf("Enter the word:");
+    scanf("%s",a);
+    for(i=0; a[i]!='\0';i++)
+    {
+        if(i=0;j=length-1;(i>j);i++;j--)
+        {
+            break;
+        }
+    }
+    if(i>j)
+    {
+        printf("not palindrome");
+    }
+    else
+    {
+        printf("palindrome");
+    }
+}
