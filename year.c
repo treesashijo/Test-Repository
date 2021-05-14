6dbfc065f6ec130457f5afacc6e8d57ae4f9af44
treesashijo <83066106+treesashijo@users.noreply.github.com>
2021-05-14 22:52:55 +0530

    Add files via upload

---
+++ year.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int y;
+    printf("Enter the year:\n");
+    scanf("%d",&y);
+    if((y%4==0 || y%400==0) && (y%100!=0))
+    {
+        printf("%d is leap year\n",y);
+    }
+    else
+    {
+        printf("%d is not a leap year\n",y);
+    }
+}
