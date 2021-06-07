8868f213beb5c78709f7398a9438135947541cfd
treesashijo <83066106+treesashijo@users.noreply.github.com>
2021-06-07 21:24:36 +0530

    Add files via upload

---
+++ root.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+double cubeRoot(double n) {
+
+    double i,precision=0.000001;
+    for(i=1; (i*i*i) <=n; ++i);
+    for(--i; (i*i*i) < n; i += precision);
+    return i;
+}
+
+int main() {
+    int n=125;
+    printf("Cube root of %d=%If",n,cubeRoot(n));
+    return 0;
+}
