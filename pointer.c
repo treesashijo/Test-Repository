843ec0babf8406a42dca8a2dda10a95bc120ab28
treesashijo <83066106+treesashijo@users.noreply.github.com>
2021-07-16 23:13:43 +0530

    Add files via upload

---
+++ pointer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int a,int b);
+int main()
+{
+    int a,b;
+    a=10;
+    b=20;
+    swap(&a,&b);//Call By REFERENCE
+    printf("In main, a=%d b=%d\n",a,b);
+}
+void swap(int a,int b);
+{
+    int t;
+    t=a;
+    a=b;
+    b=t;     
+    printf("In swap, a=%d b=%d\n",a,b);
+}
